ae8fb3ef701c66e06e980692a4d5de6b12500ad7
perlancar (on pc-office) <perlancar@gmail.com>
2017-06-08 12:56:53 +0700

    Initial commit

---
+++ lib/App/BlockWebFlooders/Spanel.pm
@@ -0,0 +1,13 @@
+package App::BlockWebFlooders::Spanel;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Block IP addresses of web flooders using iptables (on an Spanel server)
+
+=head1 SYNOPSIS
+
+See the included L<block-web-flooders-on-spanel> script.
+
+=cut
---
+++ lib/App/BlockWebFloodersOnSpanel.pm
@@ -0,0 +1,7 @@
+package App::BlockWebFloodersOnSpanel;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Dummy module to ease installation of block-web-flooders-on-spanel script
